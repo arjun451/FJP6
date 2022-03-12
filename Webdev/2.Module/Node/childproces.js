f43771d688ebf0426cc3f768cb69f309d172df54
arjun451 <aksingh25071998@gmail.com>
2022-03-13 03:55:10 +0530

    child_process

--- Webdev/2.Module/Node/childproces.js
+++ Webdev/2.Module/Node/childproces.js
@@ -1,7 +1,14 @@
 // Intro to node js
 let cp=require('child_process'); 
-console.log(cp);
+//console.log(cp);
 
 
 // Open Calucature
-cp.execFileSync("calc");
+// cp.execFileSync("calc");
+// cp.execFileSync("code");
+
+let contant = cp.execSync("node test.js");
+console.log(""+contant);
+
+
+
