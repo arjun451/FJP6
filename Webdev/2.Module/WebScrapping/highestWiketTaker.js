a4ad262269da549222af2ca8805f136a39ad4df6
arjun451 <aksingh25071998@gmail.com>
2022-08-22 04:30:31 +0530

    HighestWicket Taker webc completed

--- Webdev/2.Module/WebScrapping/highestWiketTaker.js
+++ Webdev/2.Module/WebScrapping/highestWiketTaker.js
@@ -18,8 +18,30 @@ function cb(error , response, html)
         // console.log(html);
         const dom = new JSDOM(html);
         const document  = dom.window.document;
-        let allBolower = document.querySelectorAll("ds-w-full ds-table ds-table-xs ds-table-fixed");
-        console.log(allBolower);
+        let allBolower = document.querySelectorAll(".ds-w-full.ds-table.ds-table-xs.ds-table-fixed");
+        // console.log(allBolower.length);
+        let ar = ["",-1];
+        for(let i=1;i<allBolower.length;i=i+2)
+        {
+            let row = allBolower[i].querySelectorAll("tbody tr");
+            // console.log(row.length);
+            for(let j=0;j<row.length;j++)
+            {
+                let col = row[j].querySelectorAll("td");
+                if(col.length>4)
+                {
+                    let Name = col[0].textContent;
+                    let numOfWicket = col[4].textContent;
+                    // console.log("Name of bollower--->>",  Name  ,"  Number of wicket---->>",numOfWicket);
+                    if(ar[1]<numOfWicket)
+                    {
+                      ar[0] = Name;
+                      ar[1]=numOfWicket;
+                    }
+                }
+            }
+        }
+        console.log("Highest wicket taker boller: ",ar[0] ," And Number of Wicket is: ",ar[1]);
         
 
     }
