71c6eea53d4b684ba72ae8516e262565191c6963
arjun451 <aksingh25071998@gmail.com>
2022-08-22 00:22:15 +0530

    Call back function complete

--- Webdev/2.Module/WebScrapping/callBack.js
+++ Webdev/2.Module/WebScrapping/callBack.js
@@ -1,16 +1,25 @@
-const fs= require("fs");
+const fs = require('fs');
 
-console.log("Before");
-fs.readFile("file.txt",cb);
-function cb(error,data)
-{
+
+
+console.log("Before---------");
+let contant = fs.readFileSync("request.js");
+console.log(contant+" ");
+console.log("After------");
+
+//Now letes introduced callback
+console.log("--------------Callback---------------");
+console.log("Before---------");
+fs.readFile("request.js",cb);
+ function cb(error,data)
+ {
     if(error)
     {
-        console.log("Error");
+        console.log(error);
     }
     else
     {
         console.log(data+"");
     }
-}
-console.log("After ");
+ }
+console.log("After------");
