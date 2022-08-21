37b6c87816807898147f981d6e2b2862d8185f84
arjun451 <aksingh25071998@gmail.com>
2022-08-22 02:57:45 +0530

    highest Wicket taker

---
+++ Webdev/2.Module/WebScrapping/highestWiketTaker.js
@@ -0,0 +1,27 @@
+//Basic required tools
+const request = require('request');
+const jsdom = require('jsdom');
+const {JSDOM} = jsdom;
+
+const link="https://www.espncricinfo.com/series/ipl-2021-1249214/royal-challengers-bangalore-vs-kolkata-knight-riders-eliminator-1254115/full-scorecard";
+//find the html content
+request(link,cb);
+
+function cb(error , response, html)
+{
+    if(error)
+    {
+        console.log(error);
+    }
+    else
+    {
+        // console.log(html);
+        const dom = new JSDOM(html);
+        const document  = dom.window.document;
+        let allBolower = document.querySelectorAll("ds-w-full ds-table ds-table-xs ds-table-fixed");
+        console.log(allBolower);
+        
+
+    }
+}
+
