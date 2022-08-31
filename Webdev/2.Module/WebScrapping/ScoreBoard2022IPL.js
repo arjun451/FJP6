f66ecda1137e7b967a2648a03ca66f5a14212c84
arjun451 <aksingh25071998@gmail.com>
2022-09-01 00:55:52 +0530

    ScoreBoard2022IPL project complete till json file

--- Webdev/2.Module/WebScrapping/ScoreBoard2022IPL.js
+++ Webdev/2.Module/WebScrapping/ScoreBoard2022IPL.js
@@ -1,9 +1,11 @@
 const request = require('request');
 const jsdom = require('jsdom');
 const {JSDOM} = jsdom;
-
-const link = "https://sports.ndtv.com/ipl-2022/results";
+const fs = require('fs');
+const link = "https://www.espncricinfo.com/series/ipl-2021-1249214/match-schedule-fixtures-and-results";
 request(link,cb);
+let leaboard = [];
+let count=0;
 function cb(error,response,html)
 {
     if(error)
@@ -14,8 +16,91 @@ function cb(error,response,html)
     {
     //    console.log(html);
 
-         const dom = new JSDOM(html);
-         const document = dom.window.document;
-         
+    const dom = new JSDOM(html);
+    const document  = dom.window.document;
+         let matchTag = document.querySelectorAll(".ds-grow.ds-px-4.ds-border-r.ds-border-line-default-translucent a");
+        //  console.log(matchTag.length);
+         for(let i=0;i<matchTag.length;i=i+7)
+         {
+            let matchLink = matchTag[i].href;
+            // console.log(i+" index "+matchLink);
+            let completeUrl = "https://www.espncricinfo.com"+matchLink;
+            // console.log(completeUrl);
+            request(completeUrl,cb2);
+            count++; 
+         }
+        
+
     }
 }
+function cb2(error,response,html)
+{
+    if(error)
+    {
+        console.log(error);
+    }
+    else
+    {
+        const dom1 = new JSDOM(html);
+        const document1 = dom1.window.document;
+        let playerList = document1.querySelectorAll(".ds-w-full.ds-table.ds-table-md.ds-table-auto.ci-scorecard-table tbody tr");
+        // console.log(playerList.length);
+       for(let i=0;i<playerList.length;i++)
+       {
+          let playerAttribute = playerList[i].querySelectorAll("td");
+        //   console.log(playerAttribute.length);
+        if(playerAttribute.length==8||playerAttribute.length==7)
+        {
+            let name = playerAttribute[0].textContent;
+            let runs = playerAttribute[2].textContent;
+            let balls = playerAttribute[3].textContent;
+            let fours = playerAttribute[5].textContent;
+            let sixes = playerAttribute[6].textContent;
+            // console.log("Player_Name "+name, ", Run "+run," ,ball "+ball, " ,No4s "+No4S," ,No6s "+No6s);
+            processPlayer(name,runs,balls,fours,sixes);
+           
+            // console.log(leaboard);
+        }
+
+       }
+       
+       count--;
+        if(count==0)
+        {
+            // console.log(leaboard);
+            let data = JSON.stringify(leaboard);
+            fs.writeFileSync("LeaderboardFile.json",data);
+        }
+    }
+}
+
+function processPlayer(name,runs,balls,fours,sixes)
+{
+    runs = Number(runs);
+    balls = Number(balls);
+    fours = Number(fours);
+    sixes = Number(sixes);
+    for(let i=0;i<leaboard.length;i++)
+    {
+        let playerObj = leaboard[i];
+        if(playerObj.Name == name)
+        {
+            playerObj.Runs+=runs;
+            playerObj.Balls+=balls;
+            playerObj.Fours+=fours;
+            playerObj.Sixes+=sixes;
+            playerObj.Innings+=1;
+            return ;
+        }
+    }
+    let obj = {
+        Name :name,
+        Runs :runs,
+        Balls : balls,
+        Fours :fours,
+        Sixes :sixes,
+        Innings:1
+    }
+    leaboard.push(obj);
+}
+ 
